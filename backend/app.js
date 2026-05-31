ee7aa43513b844552e8eff3d13d3abb3bbf8be06
Gisela Elia <lucena.gisela@gmail.com>
2026-05-31 13:22:05 -0400

    updates https on cors on app.jjs

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
-  "http://therian-heaven.vercel.app",
+  "https://therian-heaven.vercel.app",
 ];
 
 app.use(
